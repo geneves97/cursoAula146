c25f5668c76600ab18cef65daf545d46229013f7
George <georgeneves.dev@gmail.com>
2023-10-14 12:04:54 -0300

    feat: adiconando lombok e tostring

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -15,6 +15,9 @@ public class Program
     public static void main( String[] args )
     {
         Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENTS);
+
         System.out.println(order);
+
+        OrderStatus os1 = OrderStatus
     }
 }
